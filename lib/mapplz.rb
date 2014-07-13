e59f06df5888388def2b85747274938bfc36723c
Nick Doiron <ndoiron@mapmeld.com>
2014-07-12 20:48:10 -0400

    is limit too low?

--- lib/mapplz.rb
+++ lib/mapplz.rb
@@ -275,7 +275,7 @@ class MapPLZ
     elsif @db_type == 'mongodb'
       cursor = @db_client.command(geoNear: 'geom', near: [lat, lng], num: limit)
     elsif @db_type == 'postgis'
-      cursor = @db_client.exec("SELECT id, ST_AsText(geom) AS wkt, label, ST_Distance(start.geom::geography, ST_GeomFromText('#{wkt}')::geography) AS distance FROM mapplz AS start WHERE ST_Distance(start.geom::geography, ST_GeomFromText('#{wkt}')::geography) <= #{max} ORDER BY distance LIMIT #{limit}")
+      cursor = @db_client.exec("SELECT id, ST_AsText(geom) AS wkt, label, ST_Distance(start.geom::geography, ST_GeomFromText('#{wkt}')::geography) AS distance FROM mapplz AS start ORDER BY distance LIMIT #{limit}")
     elsif @db_type == 'spatialite'
       cursor = @db_client.execute("SELECT id, AsText(geom) AS wkt, label, Distance(start.geom, AsText('#{wkt}')) AS distance FROM mapplz AS start WHERE distance <= #{max} ORDER BY distance LIMIT #{limit}")
     end
